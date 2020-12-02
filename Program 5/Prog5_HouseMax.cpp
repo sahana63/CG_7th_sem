ae4fde67b88dd9dffe8a386829c10ba9cdac40f2
sahana63 <sahanas.cs17@rvce.edu.in>
2020-12-02 16:42:07 +0530

    code 5

--- Program 5/Prog5_HouseMax.cpp
+++ Program 5/Prog5_HouseMax.cpp
@@ -2,19 +2,17 @@
 #include<math.h>
 #include<stdio.h>
 
-
+// house coordinates
 float house[11][2] = { { 100,200 },{ 200,250 },{ 300,200 },{ 100,200 },{ 100,100 },{ 175,100 },{ 175,150 },{ 225,150 },{ 225,100 },{ 300,100 },{ 300,200 } };
 int angle;
 float m, c, theta;
 
+// Display the iniitial house
 void initialize(){
 	glClearColor(0, 0, 0, 0);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(-450, 450, -450, 450);
-	
-//NORMAL HOUSE
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	//NORMAL HOUSE
 	glColor3f(1, 0, 0);
 	glBegin(GL_LINE_LOOP);
 	for (int i = 0; i < 11; i++)
@@ -27,6 +25,7 @@ void initialize(){
 	glPushMatrix();
 }
 
+// Display the resultant house after required transformations
 void result(){
 	glColor3f(1, 1, 1);
 	glBegin(GL_LINE_LOOP);
@@ -37,34 +36,34 @@ void result(){
 	glFlush();
 }
 
-void display()
-{
+// Rotating about (100,100) by the input angle
+void rotate(){
 	initialize();
 	glTranslatef(100, 100, 0);
 	glRotatef(angle, 0, 0, 1);
 	glTranslatef(-100, -100, 0);
 	result();
 }
-void display2()
-{
+
+// Reflect the house about y=mx+c
+void reflect(){
 	initialize();
 	
+	// draw the axis y=mx+c
 	float x1 = 0, x2 = 500;
 	float y1 = m * x1 + c;
 	float y2 = m * x2 + c;
-	
 	glColor3f(1, 1, 0);
 	glBegin(GL_LINES);
-	glVertex2f(x1, y1);
-	glVertex2f(x2, y2);
+		glVertex2f(x1, y1);
+		glVertex2f(x2, y2);
 	glEnd();
 	glFlush();
 
-	//Reflected
-	glPushMatrix();
-	glTranslatef(0, c, 0);
+	// Reflection
 	theta = atan(m);
 	theta = theta * 180 / 3.14;
+	glTranslatef(0, c, 0);
 	glRotatef(theta, 0, 0, 1);
 	glScalef(1, -1, 1);
 	glRotatef(-theta, 0, 0, 1);
@@ -72,6 +71,16 @@ void display2()
 	result();
 	
 }
+
+void mouse(int btn, int state, int x, int y) {
+	if (btn == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+		rotate();
+	}
+	else if (btn == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
+		reflect();
+	}
+}
+
 void myInit() {
 	glClearColor(1.0, 1.0, 1.0, 1.0);
 	glColor3f(1.0, 0.0, 0.0);
@@ -80,16 +89,8 @@ void myInit() {
 	glLoadIdentity();
 	gluOrtho2D(-450, 450, -450, 450);
 }
-void mouse(int btn, int state, int x, int y) {
-	if (btn == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-		display();
-	}
-	else if (btn == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
-		display2();
-	}
-}
-int main(int argc, char** argv)
-{
+
+int main(int argc, char** argv){
 	printf("Enter the rotation angle\n");
 	scanf("%d", &angle);
 	printf("Enter c and m value for line y=mx+c\n");
@@ -99,7 +100,7 @@ int main(int argc, char** argv)
 	glutInitWindowSize(900, 900);
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("House Rotation");
-	glutDisplayFunc(display);
+	glutDisplayFunc(rotate);
 	glutMouseFunc(mouse); //RIGHT CLICK TO SHOW REFLECTED HOUSE
 	myInit();
 	glutMainLoop();
