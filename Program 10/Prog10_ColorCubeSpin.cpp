757e9bd021559634b98afa9f81219ada1b8c50f9
sahana63 <sahanas.cs17@rvce.edu.in>
2020-12-03 20:30:08 +0530

    minor changes

--- Program 10/Prog10_ColorCubeSpin.cpp
+++ Program 10/Prog10_ColorCubeSpin.cpp
@@ -1,7 +1,5 @@
 #include <stdlib.h>
 #include <GLUT/glut.h>
-#include <OpenGL/gl.h>
-#include <OpenGL/glu.h>
 #include <time.h>
 
 GLfloat vertices[] = { -1.0,-1.0,-1.0,1.0,-1.0,-1.0,
@@ -22,17 +20,13 @@ static GLfloat theta[] = { 0.0,0.0,0.0 };
 static GLfloat beta[] = { 0.0,0.0,0.0 };
 static GLint axis = 2;
 
-void delay(float secs)
-{
+void delay(float secs){
 	float end = clock() / CLOCKS_PER_SEC + secs;
 	while ((clock() / CLOCKS_PER_SEC) < end);
 }
 
-void displaySingle(void)
-{
-	/* display callback, clear frame buffer and z buffer,
-	   rotate cube and draw, swap buffers */
 
+void display(void){
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
 	glRotatef(theta[0], 1.0, 0.0, 0.0);
@@ -42,17 +36,14 @@ void displaySingle(void)
 	glDrawElements(GL_QUADS, 24, GL_UNSIGNED_BYTE, cubeIndices);
 
 	glBegin(GL_LINES);
-	glVertex3f(0.0, 0.0, 0.0);
-	glVertex3f(1.0, 1.0, 1.0);
+		glVertex3f(0.0, 0.0, 0.0);
+		glVertex3f(1.0, 1.0, 1.0);
 	glEnd();
-
 	glFlush();
 }
 
-void spinCube()
-{
-	/* Idle callback, spin cube 2 degrees about selected axis */
-	//sleep(50);
+// Idle callback, spin cube 2 degrees about selected axis
+void spinCube(){
 	delay(0.01);
 	theta[axis] += 2.0;
 	if (theta[axis] > 360.0) theta[axis] -= 360.0;
@@ -60,16 +51,13 @@ void spinCube()
 }
 
 
-void mouse(int btn, int state, int x, int y)
-{
-	/* mouse callback, selects an axis about which to rotate */
+void mouse(int btn, int state, int x, int y){
 	if (btn == GLUT_LEFT_BUTTON && state == GLUT_DOWN) axis = 0;
 	if (btn == GLUT_MIDDLE_BUTTON && state == GLUT_DOWN) axis = 1;
 	if (btn == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) axis = 2;
 }
 
-void myReshape(int w, int h)
-{
+void myReshape(int w, int h){
 	glViewport(0, 0, w, h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
@@ -82,16 +70,14 @@ void myReshape(int w, int h)
 	glMatrixMode(GL_MODELVIEW);
 }
 
-int main(int argc, char** argv)
-{
-	//window 1
+int main(int argc, char** argv){
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(500, 500);
 	glutCreateWindow("colorcube");
 	glutReshapeFunc(myReshape);
-	glutDisplayFunc(displaySingle);
+	glutDisplayFunc(display);
 	glutIdleFunc(spinCube);
 	glutMouseFunc(mouse);
 	glEnable(GL_DEPTH_TEST); /* Enable hidden--surface--removal */
